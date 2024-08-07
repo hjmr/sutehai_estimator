b1dd2d38362f193e877c84b4fb199591b30356b3
hjmr <2620990+hjmr@users.noreply.github.com>
2024-08-07 14:51:53 +0900

    separate model and train

--- cnn_model.py
+++ cnn_model.py
@@ -1,8 +1,4 @@
-import argparse
 import torch
-
-from paifu import load_paifu_data, make_dataset, make_dataloader, train_model, test_model
-from pai_const import code2hai
 
 
 class CnnModel(torch.nn.Module):
@@ -48,38 +44,3 @@ class CnnModel(torch.nn.Module):
         x = self.fc2(x)
         x = self.logsoftmax(x)
         return x
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", type=int, default=64, help="batch size")
-    parser.add_argument("--dev", type=str, help="device")
-    parser.add_argument("files", nargs="+", help="paifu files")
-    return parser.parse_args()
-
-
-args = parse_args()
-
-torch.device("cpu")
-if args.dev is not None:
-    device = torch.device(args.dev)
-elif torch.backends.mps.is_available():
-    device = torch.device("mps")
-print(f"device: {device}")
-
-input_data, target_data = load_paifu_data(args.files)
-dataset = make_dataset(input_data, target_data, device=device)
-train_loader, test_loader = make_dataloader(dataset, batch_size=args.batch_size)
-
-model = CnnModel(len(code2hai), device=device)
-total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print(f"# of parameters: {total_params}")
-
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-4)
-
-for epoch in range(1000):
-    loss = train_model(model, criterion, optimizer, train_loader)
-    acc = test_model(model, test_loader)
-    print(f"epoch: {epoch}, loss: {loss}, acc: {acc}")
-torch.save(model.state_dict(), "cnn_model.pth")
